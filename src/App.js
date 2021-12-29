d89875723abf2c081e34edb29586c4e65cf8b692
Atharv <atharvbhandare2001@gmail.com>
2021-12-29 19:22:18 +0530

    react hooks: useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+export default function App() {
+  const[resourceType, setResourceType] = useState('posts');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>users</button>
+        <button onClick={() => setResourceType('comments')}>comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>
+  )
 }
 
-export default App;
